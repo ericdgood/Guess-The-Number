57aea948cf3081e2e0d9a12713805667a3eb20e1
Eric Good <ericdgood@yahoo.com>
2018-09-23 19:00:27 -0400

    level 1 done besides looks.

--- app/src/main/java/com/example/edgoo/numberguess/MainActivity.java
+++ app/src/main/java/com/example/edgoo/numberguess/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.edgoo.numberguess;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     TextView highLow;
     Button guessBtn;
     Button newGameBtn;
+    Button nextLevelBtn;
     int randomNumber = (int) (Math.random() * 100) + 1;
 
     @Override
@@ -28,44 +35,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         userGuess = findViewById(R.id.guess);
         info = findViewById(R.id.info);
         guessBtn = findViewById(R.id.guessButton);
         guessLeftView = findViewById(R.id.guessLeft);
         highLow = findViewById(R.id.highLow);
         newGameBtn = findViewById(R.id.new_game_btn);
+        nextLevelBtn = findViewById(R.id.next_level_btn);
 
+//          GUESS BUTTON
+        guessBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = userGuess.getText().toString();
 
-            guessBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    smallLarger();
-                    winOrLose();
-                    guessLeft();
-                }});
+                if (value.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter a numerical guess", Toast.LENGTH_SHORT).show();;
+                } else {
+                smallLarger();
+                guessLeft();
+                userGuess.getText().clear();
+                userGuess.setHint(R.string.next_guess);
+            }}
+        });
 
-            newGameBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    newGame();
+//          NEW GAME BUTTONG
+        newGameBtn.setVisibility(View.GONE);
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newGame();
+            }
+        });
+
+        nextLevelBtn.setVisibility(View.GONE);
+        nextLevelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+//            KEEP KEYBOARD OPEN TO KEYPAD AND MAKE DONE BTN SUBMIT ANSWER
+        userGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    String value = userGuess.getText().toString();
+
+                    if (value.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "Please enter a numerical guess", Toast.LENGTH_SHORT).show();
+                    } else {
+                        smallLarger();
+                        guessLeft();
+                        userGuess.getText().clear();
+                        userGuess.setHint(R.string.next_guess);
+                    }
                 }
-            });
+                return guessLeft != 0;
+            }
+        });
 
-        }
-
-    public void winOrLose() {
-        if (guessLeft == 0) {
-            info.setText("Game Over... You Lose.\nThe random number was " + randomNumber);
-        }
     }
 
-//    method for larger or smaller
+    //    method for larger or smaller / WIN OR LOSE
+    @SuppressLint("SetTextI18n")
     public void smallLarger() {
+
         guessLeft = guessLeft - 1;
 
-        String value = userGuess.getText().toString();
-        int guess = Integer.parseInt(value);
+        if (guessLeft >= 0) {
+            String value = userGuess.getText().toString();
+            int guess = Integer.parseInt(value);
 
             if (randomNumber < guess) {
                 highLow.setText("It's smaller than " + guess + ".");
@@ -73,23 +113,34 @@ public class MainActivity extends AppCompatActivity {
                 highLow.setText("It's larger than " + guess + ".");
             } else {
                 info.setText("CORRECT.. YOU WIN!! \n The random number was " + randomNumber);
+                nextLevelBtn.setVisibility(View.VISIBLE);
+                guessBtn.setVisibility(View.GONE);
             }
+        } else {
+            info.setText("Game Over... \n The random number was " + randomNumber);
+            newGameBtn.setVisibility(View.VISIBLE);
+            guessBtn.setVisibility(View.GONE);
         }
+    }
 
-        public void guessLeft(){
-            guessLeftView.setText("You have " + guessLeft + " guesses left");
-        }
+    @SuppressLint("SetTextI18n")
+    public void guessLeft() {
+        guessLeftView.setText("You have " + guessLeft + " guesses left");
+    }
 
-        public void newGame(){
+    public void newGame() {
         randomNumber = (int) (Math.random() * 100) + 1;
         guessLeft = 10;
         info.setText("");
         guessLeftView.setText("");
         highLow.setText("");
-        }
+        newGameBtn.setVisibility(View.GONE);
+        guessBtn.setVisibility(View.VISIBLE);
+        userGuess.setHint(R.string.guess_here);
+    }
 
 //        todo: make new levels.
 //        todo: make better win or lose message.
 
-    }
+}
 
